09f196ecccd57d10656e613fae0a705b1c52b408
mariekart42 <marie.a.mensing@gmail.com>
2023-10-15 21:34:39 +0200

    leftHandNumbers and AC works

--- SWIFTY_COMPANION/SWIFTY_COMPANION/ex01_view.swift
+++ SWIFTY_COMPANION/SWIFTY_COMPANION/ex01_view.swift
@@ -1,26 +1,40 @@
 
 import SwiftUI
 
+class ButtonData {
+    var leftHandValue: String? = nil
+    var rightHandValue: String? = nil
+    var currentOperator: String? = nil
+    var display: String = "0"
+    var column: Int
+    var row: Int
+
+    init(row: Int = 0, column: Int = 0) {
+        self.row = row
+        self.column = column
+    }
+}
+
+
 struct ContentViewEx01: View
 {
     let rows = 5
     let columns = 4
     let spacing: CGFloat = 15
-    
-    @State private var display = "0"
-    @State private var scaleFactor: CGFloat = 1.0
-    @State private var currentOperator: String? // means can be nil
-//    var operationArray: [String]
+    let ButtonInstance = ButtonData()
+    @State private var realDisplay: String = "0"
     
     var body: some View
     {
         GeometryReader { proxy in
             VStack(spacing: spacing)
             {
+//                print("After initButton: \(ButtonInstance.display)")
                 Spacer()
                 HStack
                 {
-                    Text(display)
+//                    let displayNow: String = realDisplay
+                    Text(realDisplay)
                         .font(Font.system(size: proxy.size.width/CGFloat(columns)).weight(.thin)) // Set the font size to 30 points
                         .minimumScaleFactor(0.6) // Adjust the minimum scale factor as needed
                         .lineLimit(1) // everything will be dispalyed on 0ne line
@@ -29,55 +43,24 @@ struct ContentViewEx01: View
                         .onAppear
                         {
                             // TODO: here add later, if text is too big, dont change previuse text(only for typing numbers, not calculating and displayin!)
-                            let textSize = display.size(withAttributes: [.font: UIFont.systemFont(ofSize: proxy.size.width / CGFloat(columns))])
-                            scaleFactor = proxy.size.width / (CGFloat(columns) * textSize.width)
+                            let textSize = ButtonInstance.display.size(withAttributes: [.font: UIFont.systemFont(ofSize: proxy.size.width / CGFloat(columns))])
+                            let scaleFactor: CGFloat = proxy.size.width / (CGFloat(columns) * textSize.width)
                             
                             if scaleFactor < 0.6 {
                                 print("Text is too big and is getting resized beyond the specified minimumScaleFactor.")
                             }
                         }
-
-
-
                 }.padding(spacing) // distance between upper Spacer and buttons
                 ForEach(0..<rows, id: \.self) { row in
                     HStack(spacing: spacing)
                     {
                         ForEach(0..<Int(columns), id: \.self) { column in
                             roundNumberButton(currentRow: row, currentColumn: column, width: getWidth(proxy: proxy), myAction: {
-                                // action
-                                
-                                
-                                
-                                let cgFloatResult: CGFloat = myStoCGFloat(result: display)
-                                let stringButtonValue: String = getValueAt(row: row, column: column)
-                                let cgFloatButtonValue: CGFloat = myStoCGFloat(result: stringButtonValue)
-
-//                                if (
-                                
-                                
-                                
-//                                if (buttonIsNumber(value: cgFloatButtonValue)) {
-//                                    if (currentOperator != nil) {
-//                                        // we are d
-//                                    }
-//                                    if (cgFloatButtonValue == 0 && cgFloatResult == 0) {
-//                                        print("i will do nothing lol")
-//                                    } else if (cgFloatResult == 0) {
-//                                        display = stringButtonValue
-//                                        operationArray.removeAll()
-//                                        operationArray.append(stringButtonValue)
-//                                    } else {
-//                                        display += stringButtonValue
-//                                    }
-//                                } else if (stringButtonValue == "AC") {
-//                                    display = "0";
-//                                    operationArray.removeAll()
-//                                } else if (buttonIsOperator(value: stringButtonValue)) {
-//                                    currentOperator = stringButtonValue
-//                                } else {
-//                                    print("not implemented yet")
-//                                }
+                                ButtonInstance.row = row
+                                ButtonInstance.column = column
+                                initButtonValues(instance: ButtonInstance, pressedButton: getValueAt(row: row, column: column))
+                                print("After initButton: \(ButtonInstance.display)")
+                                realDisplay = ButtonInstance.display
                             })
                         }
                     }
@@ -85,37 +68,30 @@ struct ContentViewEx01: View
             }
         }.padding(spacing)// distance between screen edge and buttons
     }
-
     
-    func buttonIsOperator(value: String) -> Bool {
-        if value == "÷" || value == "×" || value == "−" || value == "+" {
-            return true
-        }
-        return false
-    }
-    
-    func buttonIsNumber(value: CGFloat) -> Bool {
-        if value >= 0 && value <= 9 {
-            return true // its a number
-        }
-        return false // if nil or not a number
-    }
     
     func getWidth(proxy: GeometryProxy) -> CGFloat {
-        let typecastColumns: CGFloat = CGFloat(columns)
-        
-        return (proxy.size.width/typecastColumns)-spacing*0.75
+        let columnsCGFloat: CGFloat = CGFloat(columns)
+        return (UIScreen.main.bounds.width - (columnsCGFloat + 1) * spacing) / columnsCGFloat
     }
 // end of struct
 }
 
 
 
+func buttonIsNumber(value: CGFloat) -> Bool {
+    if value >= 0 && value <= 9 {
+        return true // its a number
+    }
+    return false // if nil or not a number
+}
 
-
-
-
-
+func buttonIsOperator(value: String) -> Bool {
+    if value == "÷" || value == "×" || value == "−" || value == "+" {
+        return true
+    }
+    return false
+}
 
 
 
@@ -163,3 +139,109 @@ func getBackgroundColor(currentRow: Int, currentColumn: Int) -> Color {
     }
 }
 
+
+// INIT THESE:
+//    instance.display
+//    instance.leftHandValue
+//    instance.rightHandValue
+//    instance.currentOperator
+func initButtonValues(instance: ButtonData, pressedButton: String) -> Void {
+    print("lol: \(instance.row) | \(instance.column)")
+    
+    let buttonValueCFFloat: CGFloat = myStoCGFloat(result: pressedButton)
+    
+    var tmpLeftHand: String = instance.leftHandValue ?? "0" // if leftHandValue is nil -> assigns it to "0"
+    var tmpRightHand: String = instance.rightHandValue ?? "0" // if rightHandValue is nil -> assigns it to "0"
+
+    if (pressedButton == "AC") {
+        instance.currentOperator = nil
+        instance.rightHandValue = nil
+        instance.leftHandValue = nil
+        instance.display = "0"
+        return
+    }
+    else if (buttonIsNumber(value: buttonValueCFFloat))
+    {
+        if (instance.display == "0" && buttonValueCFFloat == 0)
+        {
+            instance.display = "0"
+        }
+        else if (instance.currentOperator == nil)
+        {
+            if (tmpLeftHand == "0")
+            {
+                instance.leftHandValue = pressedButton
+                instance.display = pressedButton
+                return
+            }
+            else
+            {
+                tmpLeftHand += pressedButton
+                instance.leftHandValue = tmpLeftHand
+                instance.display = tmpLeftHand
+                return
+            }
+        }
+        else if (instance.currentOperator != nil)
+        {
+            if (tmpRightHand == "0")
+            {
+                instance.rightHandValue = pressedButton
+                instance.display = pressedButton
+                return
+            }
+            else
+            {
+                tmpRightHand += pressedButton
+                instance.leftHandValue = tmpRightHand
+                instance.display = tmpRightHand
+                return
+            }
+        }
+    }
+    
+}
+
+
+//                                if (buttonIsNumber(value: cgFloatButtonValue)) {
+//                                    if (currentOperator != nil) {
+//                                        // we are d
+//                                    }
+//                                    if (cgFloatButtonValue == 0 && cgFloatResult == 0) {
+//                                        print("i will do nothing lol")
+//                                    } else if (cgFloatResult == 0) {
+//                                        display = stringButtonValue
+//                                        operationArray.removeAll()
+//                                        operationArray.append(stringButtonValue)
+//                                    } else {
+//                                        display += stringButtonValue
+//                                    }
+//                                } else if (stringButtonValue == "AC") {
+//                                    display = "0";
+//                                    operationArray.removeAll()
+//                                } else if (buttonIsOperator(value: stringButtonValue)) {
+//                                    currentOperator = stringButtonValue
+//                                } else {
+//                                    print("not implemented yet")
+//                                }
+
+
+//                                let displayValueCGFloat: CGFloat = myStoCGFloat(result: ButtonInstance.display)
+//                                let buttonValueString: String = getValueAt(row: row, column: column)
+//                                let buttonValueCFFloat: CGFloat = myStoCGFloat(result: buttonValueString)
+//
+//                                if (buttonValueString == "AC") {
+//                                    display = "0"
+//                                    ButtonInstance.leftHandValue = nil
+//                                    ButtonInstance.rightHandValue = nil
+//                                    ButtonInstance.currentOperator = nil
+//                                } else if (buttonIsNumber(value: buttonValueCFFloat)) {
+//                                    display = getDisplayValue()
+//                                    if (currentOperator == nil) {
+//                                        ButtonInstance.rightHandValue = getRightHandValue()
+//                                    } else {
+//                                        leftHandValue = getLeftHandValue()
+//                                    }
+//                                    if (currentOperator == nil) {
+//                                        if (displayValueCGFloat == 0 && buttonValueCFFloat == 0) {
+//                                            display = "0"
